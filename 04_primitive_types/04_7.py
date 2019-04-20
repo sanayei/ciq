2ab78ccfd14991bda3bb8f4eab7b182cc15144bb
Amir Sanayei <Amir.Sanayei@wdc.com>
2019-04-19 23:55:08 -0700

    added code for x to power of y

---
+++ 04_primitive_types/04_7.py
@@ -0,0 +1,22 @@
+'''
+Write a program that takes double x and integer y and returns x to the power of y
+'''
+
+def power(x, y):
+    r = 1
+    while y:
+        if y & 1:
+            r = r * x
+        x = x * x
+        y = y >> 1
+    return r
+
+
+
+
+
+if __name__ == '__main__':
+    x = 2
+    y = 5
+
+    print(x, y, power(x,y))
