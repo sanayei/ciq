4b50147d67181a9bd61f66bf7a86301acff71f1a
Amir Sanayei <Amir.Sanayei@wdc.com>
2019-04-19 23:44:07 -0700

    clean up

--- 13_sorting/quick_sort.py
+++ 13_sorting/quick_sort.py
@@ -1,28 +1,29 @@
+'''
+Code for quick sort
+'''
 
 
-
-
-def partition(a,low, high):
+def partition(a, low, high):
     pivot = a[high]
     i = low
     for j in range(low, high):
         if pivot >= a[j]:
-            a[i], a[j] = a[j] , a[i]
+            a[i], a[j] = a[j], a[i]
             i += 1
     a[i], a[high] = a[high], a[i]
     return i
 
-def _quicksort(a,low, high):
+
+def _quicksort(a, low, high):
     if low < high:
         p = partition(a, low, high)
-        _quicksort(a, low, p-1)
-        _quicksort(a, p+1, high)
+        _quicksort(a, low, p - 1)
+        _quicksort(a, p + 1, high)
     return a
 
 
 def quicksort(a):
-    return _quicksort(a, 0, len(a)-1)
-
+    return _quicksort(a, 0, len(a) - 1)
 
 
 if __name__ == '__main__':
