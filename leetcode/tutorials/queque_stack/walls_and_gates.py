14602346fd2f1db2844d400e8b67c02b08944180
Amir Sanayei <Amir.Sanayei@wdc.com>
2019-06-15 12:30:43 -0700

    walls and gates

--- leetcode/tutorials/queque_stack/walls_and_gates.py
+++ leetcode/tutorials/queque_stack/walls_and_gates.py
@@ -1,5 +1,4 @@
-from collections import deque
-
+import collections
 WALL = -1
 GATE = 0
 EMPTY = 2147483647
@@ -9,41 +8,36 @@ def wallsAndGates(rooms):
     """
     Do not return anything, modify rooms in-place instead.
     """
-    m = len(rooms) - 1
-    n = len(rooms[0]) - 1
+    if not rooms or len(rooms) == 0:
+        return
+    nrows = len(rooms)
+    ncols = len(rooms[0])
 
     def findNeighbors(point):
         x = point[0]
         y = point[1]
         return [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
 
-    def findDistanceToGate(r, c):
-        disntances = [[0] * n] * m
-        print('size of distances =', m, n)
-        q = deque()
-        q.append((r, c))
-        while len(q) > 0:
-            p = q.popleft()
-            for point in findNeighbors(p):
-                x = point[0]
-                y = point[1]
-                if x < 0 or y < 0 or x > m or y > n or rooms[x][y] == WALL or disntances[x][y] != 0:
-                    pass
-                else:
-                    disntances[x][y] += 1
-                    if rooms[x][y] == GATE:
-                        return disntances[x][y]
-                    q.append(point)
-        return EMPTY
+    q = collections.deque()
+    for i in range(nrows):
+        for j in range(ncols):
+            if rooms[i][j] == GATE:
+                q.append((i, j))
 
-    for i in range(m + 1):
-        for j in range(n + 1):
-            if rooms[i][j] == EMPTY:
-                rooms[i][j] = findDistanceToGate(i, j)
-
-
+    while len(q) > 0:
+        p = q.popleft()
+        for r, c in findNeighbors(p):
+            if 0 <= r < nrows and 0 <= c < ncols and rooms[r][c] == EMPTY:
+                rooms[r][c] = rooms[p[0]][p[1]] + 1
+                q.append((r, c))
+    return rooms
 
 if __name__ == '__main__':
-    input = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
 
-    print(wallsAndGates(input))
+
+    rooms = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
+
+    print(wallsAndGates(rooms))
+
+
+
