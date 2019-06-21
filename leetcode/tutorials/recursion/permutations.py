b01058f3a5d0f9650655ce437e43146f0a250b60
Amir Sanayei <Amir.Sanayei@wdc.com>
2019-06-20 22:21:37 -0700

    another question

---
+++ leetcode/tutorials/recursion/permutations.py
@@ -0,0 +1,17 @@
+def permutation(lst):
+    output = []
+    n = len(lst)
+    def backtrack(sidx=0):
+        if sidx == n:
+            output.append(lst.copy())
+        for i in range(sidx, n):
+            lst[i], lst[sidx] = lst[sidx], lst[i]
+            backtrack(i + 1)
+            lst[i], lst[sidx] = lst[sidx], lst[i]
+    backtrack()
+
+
+if __name__ == '__main__':
+
+    a = [1, 2, 3, 4, 5, 6]
+    print(permutation(a))
