e758d3745248da7bc4f2b34aabf73f4b7f1e3d83
Amir Sanayei <asanayei@gmail.com>
2019-06-30 19:52:01 -0700

    another question

--- leetcode/medium/construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ leetcode/medium/construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -14,3 +14,24 @@
 #   9  20
 #     /  \
 #    15   7
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def buildTree(preorder, inorder):
+    """
+    :type preorder: List[int]
+    :type inorder: List[int]
+    :rtype: TreeNode
+    """
+    if not inorder:
+        return None
+    ind = inorder.index(preorder.pop(0))
+    root = TreeNode(inorder[ind])
+    root.left = buildTree(preorder, inorder[:ind])
+    root.right = buildTree(preorder, inorder[ind + 1:])
+    return root
