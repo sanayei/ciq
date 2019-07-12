3452d6dbe8eee37678715d49e082ac685611bfe9
Amir Sanayei <asanayei@gmail.com>
2019-07-11 23:48:18 -0700

    another question

---
+++ leetcode/MEDIUM_find_leaves_of_binary_tree.py
@@ -0,0 +1,19 @@
+# Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
+#
+# Example:
+#
+# Input: [1, 2, 3, 4, 5]
+#
+# Input: [1, 2, 3, 4, 5]
+#
+#         1
+#         / \
+#             2
+#     3
+#     / \
+#         4
+# 5
+#
+# Output: [[4, 5, 3], [2], [1]]
+#
+
