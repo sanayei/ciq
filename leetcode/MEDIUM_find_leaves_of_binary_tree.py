fba3f8a970a611947549812e00d68d3b2569aa8b
Amir Sanayei <asanayei@gmail.com>
2019-07-12 07:58:15 -0700

    another question

--- leetcode/MEDIUM_find_leaves_of_binary_tree.py
+++ leetcode/MEDIUM_find_leaves_of_binary_tree.py
@@ -17,3 +17,17 @@
 # Output: [[4, 5, 3], [2], [1]]
 #
 
+def findLeaves(root):
+    from collections import defaultdict
+    height = defaultdict(list)
+
+    def helper(node):
+        if node is None:
+            return 0
+        else:
+            h = 1 + max(helper(node.left), helper(node.right))
+        height[h].append(node.val)
+        return h
+
+    helper(root)
+    return [height[i] for i in height.keys()]
