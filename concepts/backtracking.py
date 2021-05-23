a835810218e01595e220735198922b499a59aef5
Amir Sanayei <amir.sanayei@wdc.com>
2021-05-23 12:36:08 -0700

    added new backtracking

--- concepts/backtracking.py
+++ concepts/backtracking.py
@@ -1,66 +1,59 @@
-# find all combinations of size k of a set
-def combinations(lst, k):
-    output = []
-    n = len(lst)
-    def backtrack(start_idx=0, curr=[]):
-        if len(curr) == k:
-            output.append(curr.copy())
-        for i in range(start_idx, n):
-            curr.append(lst[i])
-            backtrack(i+1, curr)
-            curr.pop()
-
-    backtrack()
-    return output
-
-# find all subsets of a given set
-def subsets(lst):
-    n = len(lst)
-    output = []
-    for i in range(n+1):
-        output.extend(combinations(lst,i))
-    return output
+from typing import *
 
 
-def permutation2(lst):
-    output = []
-    n = len(lst)
-    def backtrack(sidx=0):
-        if sidx == n:
-            output.append(lst.copy())
-        for i in range(sidx, n):
-            lst[i], lst[sidx] = lst[sidx], lst[i]
-            backtrack(sidx + 1)
-            lst[i], lst[sidx] = lst[sidx], lst[i]
-    backtrack()
-    return output
+def combination(nums, k):
+    res = []
+
+    def backtrack(chosen, options, depth):
+        if depth == 0:
+            res.append(chosen[:])
+        else:
+            for i, opt in enumerate(options):
+                chosen.append(opt)
+                backtrack(chosen, options[i + 1:], depth - 1)
+                chosen.pop()
+
+    backtrack([], nums, k)
+    return res
 
 
-def allpermutation(lst):
-    s = set()
+def permutation(nums, k):
+    res = []
 
-    def dfs(path, t):
-        if path:
-            s.add(path)
-        for i in range(len(t)):
-            dfs(path+t[i], t[:i]+t[i+1:])
+    def backtrack(chosen, options):
+        if len(chosen) == k:
+            res.append(chosen[:])
+        else:
+            for opt in options:
+                if opt in chosen:
+                    pass
+                else:
+                    chosen.append(opt)
+                    backtrack(chosen, options)
+                    chosen.pop()
 
-    dfs('', lst)
-    return s
+    backtrack([], nums)
+    return res
 
-def permutation1(lst):
-    s = set()
-    n = len(lst)
-    def dfs(path, t):
-        if len(path) == n:
-            s.add(path)
-        for i in range(len(t)):
-            dfs(path+t[i], t[:i]+t[i+1:])
-    dfs('', lst)
-    return s
 
+def subsets(nums):
+    res = []
+
+    def backtrack(chosen, options):
+        res.append(chosen[:])
+        for i, opt in enumerate(options):
+            chosen.append(opt)
+            backtrack(chosen, options[i + 1:])
+            chosen.pop()
+
+    backtrack([], nums)
+    return res
 
 
 if __name__ == '__main__':
-    nums = '123'
-    print(permutation2(nums))
+    r = combination([1, 2, 3], 1)
+    s = subsets([1, 2, 3])
+    p = permutation([1, 2, 3], 3)
+    print(r)
+    print(s)
+    print(p)
